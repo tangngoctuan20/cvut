28240be6b10e12e9b52f34012fa782dede689640
TuanTNG <tangngoctuantttt@gmail.com>
2021-03-21 07:11:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
 	name="cvut",
 	version='0.0.1',
 	packages=setuptools.find_packages(),
-	install_requires=[
-		'cython', 'numpy', 'opencv-python', 'sklearn', 'pandas',
-		'ipython', 'pylint', 'ipympl', 'pyyaml', 'tqdm', 'future',
-	],
+# 	install_requires=[
+# 		'cython', 'numpy', 'opencv-python', 'sklearn', 'pandas',
+# 		'ipython', 'pylint', 'ipympl', 'pyyaml', 'tqdm', 'future',
+# 	],
 	python_requires='>=3.6',
 )
